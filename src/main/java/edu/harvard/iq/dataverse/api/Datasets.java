f01a099a1189c03eb7adeb0841c19563247aeb47
qqmyers <qqmyers@hotmail.com>
2025-09-09 15:42:34 -0400

    update latest version jsonld call

--- src/main/java/edu/harvard/iq/dataverse/api/Datasets.java
+++ src/main/java/edu/harvard/iq/dataverse/api/Datasets.java
@@ -934,8 +934,8 @@ public class Datasets extends AbstractApiBean {
     @AuthRequired
     @Path("{id}/metadata")
     @Produces("application/ld+json, application/json-ld")
-    public Response getJsonLDMetadata(@Context ContainerRequestContext crc, @PathParam("id") String id, @Context UriInfo uriInfo, @Context HttpHeaders headers) {
-        return getVersionJsonLDMetadata(crc, id, DS_VERSION_LATEST, uriInfo, headers);
+    public Response getJsonLDMetadata(@Context ContainerRequestContext crc, @PathParam("id") String id, @QueryParam("includeFiles") boolean includeFiles, @Context UriInfo uriInfo, @Context HttpHeaders headers) {
+        return getVersionJsonLDMetadata(crc, id, DS_VERSION_LATEST, includeFiles, uriInfo, headers);
     }
 
     @PUT
